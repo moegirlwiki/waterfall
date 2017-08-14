64864a440b748424a97ae076e683632eff3cd523
Bingxing Wang <i@imbushuo.net>
2017-08-14 11:27:24 -0700

    Add initial project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Waterfall Layout for Moegirlpedia
+
+This project aims to implement web-based waterfall layout in Moegirlpedia for recent changes/new pages visualization.
---
+++ src/TheLittleMoeNewLlc.MgWaterfall/DataObjects/RpcEndpointConfiguration.cs
@@ -0,0 +1,11 @@
+﻿// RpcEndpointConfiguration.cs: Data model that represents RPC endpoint configuration
+// File Owner: Bingxing Wang <i@imbushuo.net>
+// Copyright 2017 The Little Moe New LLC. All rights reserved.
+
+namespace TheLittleMoeNewLlc.MgWaterfall.DataObjects
+{
+    public class RpcEndpointConfiguration
+    {
+        public string ApiEndpoint { get; set; }
+    }
+}
