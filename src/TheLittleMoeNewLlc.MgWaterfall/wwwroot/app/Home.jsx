3b35c38b12686632e628037f8de0fb4adaa36a38
Bingxing Wang <i@imbushuo.net>
2017-08-14 13:48:23 -0700

    Add initial card layout with demo data.

--- src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/Home.jsx
+++ src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/Home.jsx
@@ -1,38 +1,68 @@
 ﻿var Masonry = require('react-masonry-component');
 
-var CommentList = React.createClass({
+var masonryOptions = {
+    transitionDuration: 0
+};
+
+var Gallery = React.createClass({
     render: function () {
+        var childElements = this.props.elements.map(function (element) {
+            return (
+                <div className="card" key={element.key}>
+                    <img className="card-cover" src={element.src} />
+                    <h1 className="card-title">{element.title}</h1>
+                </div>
+            );
+        });
+
         return (
-            <div className="commentList">
-                Hello, world! I am a CommentList.
-      </div>
+            <Masonry
+                className={'articles-gallery'} // default ''
+                elementType={'div'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+            >
+                {childElements}
+            </Masonry>
         );
     }
 });
 
-var CommentForm = React.createClass({
-    render: function () {
-        return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-      </div>
-        );
+var cards = [
+    {
+        key: '1edf2ada-dd06-4d83-afa7-b4f01bdacfc5',
+        title: 'Forza Horizon 3',
+        src: 'https://commons.moegirl.org/thumb.php?f=Forza_Horizon_3_Cover.jpg&width=250'
+    },
+    {
+        key: '3accdb6a-f4b6-4dca-a0d6-3e13fc345acf',
+        title: 'Gear Of Wars 4',
+        src: 'https://img.moegirl.org/common/thumb/2/22/Gears_Of_War_4_Store_Preliminary_Background.jpg/250px-Gears_Of_War_4_Store_Preliminary_Background.jpg'
+    },
+    {
+        key: '0cf48b0a-2faa-4ecd-a0e6-a931f91d5fd0',
+        title: 'Forza Motorsport 7',
+        src: 'https://commons.moegirl.org/thumb.php?f=Forza_Motorsport_7_Cover.jpg&width=250'
+    },
+    {
+        key: '46c49ab6-06a8-435d-bf4a-2022738980de',
+        title: 'Halo 4',
+        src: 'https://img.moegirl.org/common/thumb/4/46/Halo_4_Cover.jpg/250px-Halo_4_Cover.jpg'
+    },
+    {
+        key: 'f1244d54-d148-48f0-887f-2d5dd311e26f',
+        title: 'Halo Wars 2',
+        src: 'https://img.moegirl.org/common/thumb/9/9e/Halo_Wars_2_Cover.png/250px-Halo_Wars_2_Cover.png'
+    },
+    {
+        key: '7aceeb53-71c7-4a76-b038-0fe641e08168',
+        title: 'Halo The Master Chief Collection',
+        src: 'https://commons.moegirl.org/thumb.php?f=Halo_The_Master_Chief_Collection_Cover.png&width=250'
     }
-});
-
-var CommentBox = React.createClass({
-    render: function () {
-        return (
-            <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList />
-                <CommentForm />
-            </div>
-        );
-    }
-});
+];
 
 ReactDOM.render(
-    <CommentBox />,
-    document.getElementById('content')
+    <Gallery elements={cards} />,
+    document.getElementById('card-container')
 );
--- src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/home.bundle.js
+++ src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/home.bundle.js
@@ -900,41 +900,71 @@ module.exports = React;
 
 /** @jsx React.DOM */var Masonry = __webpack_require__(12);
 
-var CommentList = React.createClass({displayName: "CommentList",
-    render: function () {
-        return (
-            React.createElement("div", {className: "commentList"}, 
-                "Hello, world! I am a CommentList."
-      )
-        );
-    }
-});
+var masonryOptions = {
+    transitionDuration: 0
+};
 
-var CommentForm = React.createClass({displayName: "CommentForm",
+var Gallery = React.createClass({displayName: "Gallery",
     render: function () {
-        return (
-            React.createElement("div", {className: "commentForm"}, 
-                "Hello, world! I am a CommentForm."
-      )
-        );
-    }
-});
+        var childElements = this.props.elements.map(function (element) {
+            return (
+                React.createElement("div", {className: "card", key: element.key}, 
+                    React.createElement("img", {className: "card-cover", src: element.src}), 
+                    React.createElement("h1", {className: "card-title"}, element.title)
+                )
+            );
+        });
 
-var CommentBox = React.createClass({displayName: "CommentBox",
-    render: function () {
         return (
-            React.createElement("div", {className: "commentBox"}, 
-                React.createElement("h1", null, "Comments"), 
-                React.createElement(CommentList, null), 
-                React.createElement(CommentForm, null)
+            React.createElement(Masonry, {
+                className: 'articles-gallery', // default ''
+                elementType: 'div', // default 'div'
+                options: masonryOptions, // default {}
+                disableImagesLoaded: false, // default false
+                updateOnEachImageLoad: false// default false and works only if disableImagesLoaded is false
+            }, 
+                childElements
             )
         );
     }
 });
 
+var cards = [
+    {
+        key: '1edf2ada-dd06-4d83-afa7-b4f01bdacfc5',
+        title: 'Forza Horizon 3',
+        src: 'https://commons.moegirl.org/thumb.php?f=Forza_Horizon_3_Cover.jpg&width=250'
+    },
+    {
+        key: '3accdb6a-f4b6-4dca-a0d6-3e13fc345acf',
+        title: 'Gear Of Wars 4',
+        src: 'https://img.moegirl.org/common/thumb/2/22/Gears_Of_War_4_Store_Preliminary_Background.jpg/250px-Gears_Of_War_4_Store_Preliminary_Background.jpg'
+    },
+    {
+        key: '0cf48b0a-2faa-4ecd-a0e6-a931f91d5fd0',
+        title: 'Forza Motorsport 7',
+        src: 'https://commons.moegirl.org/thumb.php?f=Forza_Motorsport_7_Cover.jpg&width=250'
+    },
+    {
+        key: '46c49ab6-06a8-435d-bf4a-2022738980de',
+        title: 'Halo 4',
+        src: 'https://img.moegirl.org/common/thumb/4/46/Halo_4_Cover.jpg/250px-Halo_4_Cover.jpg'
+    },
+    {
+        key: 'f1244d54-d148-48f0-887f-2d5dd311e26f',
+        title: 'Halo Wars 2',
+        src: 'https://img.moegirl.org/common/thumb/9/9e/Halo_Wars_2_Cover.png/250px-Halo_Wars_2_Cover.png'
+    },
+    {
+        key: '7aceeb53-71c7-4a76-b038-0fe641e08168',
+        title: 'Halo The Master Chief Collection',
+        src: 'https://commons.moegirl.org/thumb.php?f=Halo_The_Master_Chief_Collection_Cover.png&width=250'
+    }
+];
+
 ReactDOM.render(
-    React.createElement(CommentBox, null),
-    document.getElementById('content')
+    React.createElement(Gallery, {elements: cards}),
+    document.getElementById('card-container')
 );
 
 /***/ }),
