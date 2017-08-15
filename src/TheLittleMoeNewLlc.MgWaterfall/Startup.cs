ca51a4364524c7565cc657e4040850be6c0f3c34
Bingxing Wang <i@imbushuo.net>
2017-08-14 22:42:59 -0700

    Enable markup minification.

--- src/TheLittleMoeNewLlc.MgWaterfall/Startup.cs
+++ src/TheLittleMoeNewLlc.MgWaterfall/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TheLittleMoeNewLlc.MgWaterfall.DataObjects;
 
 using React.AspNet;
-using Microsoft.AspNetCore.Http;
+using WebMarkupMin.AspNetCore1;
 
 namespace TheLittleMoeNewLlc.MgWaterfall
 {
@@ -38,6 +35,10 @@ namespace TheLittleMoeNewLlc.MgWaterfall
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddReact();
 
+            // Add WebMarkupMin services.
+            services.AddWebMarkupMin()
+                    .AddHtmlMinification();
+
             // Add framework services.
             services.AddMvc();
         }
@@ -56,31 +57,10 @@ namespace TheLittleMoeNewLlc.MgWaterfall
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseWebMarkupMin();
             }
 
-            // Initialise ReactJS.NET. Must be before static files.
-            app.UseReact(config =>
-            {
-                // If you want to use server-side rendering of React components,
-                // add all the necessary JavaScript files here. This includes
-                // your components as well as all of their dependencies.
-                // See http://reactjs.net/ for more information. Example:
-                //config
-                //  .AddScript("~/Scripts/First.jsx")
-                //  .AddScript("~/Scripts/Second.jsx");
-
-                // If you use an external build too (for example, Babel, Webpack,
-                // Browserify or Gulp), you can improve performance by disabling
-                // ReactJS.NET's version of Babel and loading the pre-transpiled
-                // scripts. Example:
-                //config
-                //  .SetLoadBabel(false)
-                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
-
-                // React Masonry Component
-
-            });
-
+            app.UseReact(config => { });
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
