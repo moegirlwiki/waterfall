144d6cc6dd5e3c575244f69b1c2e81a36034f033
Bingxing Wang <i@imbushuo.net>
2017-08-14 22:15:21 -0700

    Implement initial incremental load.

--- src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/Home.jsx
+++ src/TheLittleMoeNewLlc.MgWaterfall/wwwroot/app/Home.jsx
@@ -1,4 +1,5 @@
 ﻿var Masonry = require('react-masonry-component');
+var VisibilitySensor = require('react-visibility-sensor');
 
 var masonryOptions = {
     transitionDuration: '0.4s',
@@ -34,24 +35,53 @@ var RecentNewPagesHost = React.createClass({
     getInitialState: function () {
         return {
             pages: [],
-            continuationToken: null
+            continuationToken: null,
+            isLoading: false
         };
     },
     componentWillMount: function () {
+        // Redirect
+        this.loadData();
+    },
+    loadData: function () {
         var xhr = new XMLHttpRequest();
-        xhr.open('get', '/NewPageQuery', true);
+        var endpoint = '/NewPageQuery';
+
+        if (this.state.continuationToken) {
+            endpoint = '/NewPageQuery?id=' + encodeURIComponent(this.state.continuationToken);
+        }
+
+        xhr.open('get', endpoint, true);
         xhr.onload = function () {
             var rel = JSON.parse(xhr.responseText);
-            this.setState({
-                pages: rel.pages,
-                continuationToken: rel.continuationToken
-            });
+            if (this.state.continuationToken) {
+                this.setState({
+                    pages: this.state.pages.concat(rel.pages),
+                    continuationToken: rel.continuationToken
+                });
+            } else {
+                this.setState({
+                    pages: rel.pages,
+                    continuationToken: rel.continuationToken
+                });
+            }
         }.bind(this);
+
         xhr.send();
+    },
+    onVisibilityChange: function (isVisible) {
+        // Load more if loaded before
+        if (isVisible && this.state.continuationToken) {
+            console.info("Load more data starting from " + this.state.continuationToken);
+            this.loadData();
+        }
     },
     render: function () {
         return (
-            <CardGallery elements={this.state.pages} />
+            <div>
+                <CardGallery elements={this.state.pages} />
+                <VisibilitySensor onChange={this.onVisibilityChange} />
+            </div>
         );
     }
 });
